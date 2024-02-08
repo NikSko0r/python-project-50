3071dbaab63836682df4e38a9b7433d819ab25b8
Nikolay Skorokhod <mrsuperpuper016@gmail.com>
2024-02-08 23:28:16 +0300

    step3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/NikSko0r/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/NikSko0r/python-project-50/actions)
+
+
+### asciinema generate_diff function in cli:
+[![asciicast](https://asciinema.org/a/LZC2gfOCIfyQe8PY67EgK72Vm.svg)](https://asciinema.org/a/LZC2gfOCIfyQe8PY67EgK72Vm)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,21 +1,53 @@
 #! usr/bin/env python3
 
 import argparse
+import json
 
 
-def help_inform():
+def is_key(key, dic):
+    return key in dic
+
+
+def generate_diff(file1, file2):
+    with open(file1, encoding='utf-8') as file1, open(file2, encoding='utf-8') as file2:
+        dic1, dic2 = json.load(file1), json.load(file2)
+    lst = sorted({**dic1, **dic2})
+    string = ''
+    for i in lst:
+        if is_key(i, dic1) and is_key(i, dic2):
+            if dic1[i] == dic2[i]:
+                string += f'    {i}: {dic1[i]}\n'
+            else:
+                string += f'  - {i}: {dic1[i]}\n'
+                string += f'  + {i}: {dic2[i]}\n'
+        else:
+            if is_key(i, dic1):
+                string += f'  - {i}: {dic1[i]}\n'
+            else:
+                string += f'  + {i}: {dic2[i]}\n'
+    string = '{\n' + string + '}'
+    print(string)
+
+
+def gendiff():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.',)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args.first_file, args.second_file)
+    generate_diff(args.first_file, args.second_file)
 
 
 def main():
-    help_inform()
+    gendiff()
 
 
 if __name__ == '__main__':
     main()
+
+__all__ = (
+    'generate_diff',
+    'gendiff',
+    'is_key',
+)
